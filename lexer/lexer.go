724823f7babbdeff5a4cd942817335122ca8b0af
nemosho <shota.nemoto@gmail.com>
2019-10-26 20:34:55 +0900

    Add to skip whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -28,6 +28,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) nextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -74,6 +76,13 @@ func (l *Lexer) readIdentifier() string {
 		l.readChar()
 	}
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+
 }
 
 func isLetter(ch byte) bool {
