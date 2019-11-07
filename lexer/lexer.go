d64036503cda55f1e3f9719da0c922a0c1ebefed
nemosho <shota.nemoto@gmail.com>
2019-11-08 00:40:41 +0900

    Fix nextToken method to public (NextToken())

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,7 +25,7 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
-func (l *Lexer) nextToken() token.Token {
+func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
 	l.skipWhitespace()
