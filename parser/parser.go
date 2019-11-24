dbd798daf11935663d95387343174f3a67d3f761
nemosho <shota.nemoto@gmail.com>
2019-11-24 16:39:58 +0900

    Fix reference to member

--- parser/parser.go
+++ parser/parser.go
@@ -34,7 +34,7 @@ func (p *Parser) Errors() []string {
 func (p *Parser) peekError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
 		t, p.peekToken.Type)
-	p.errors = append(p.erors, msg)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
